9f6bb7089bf6db77ab2aa11858b9762c18414107
Ansh411 <4gansh11@gmail.com>
2025-10-18 00:28:17 +0530

    created user schema

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+
+// extract styles and scripts from sub pages into the layout
+
+app.set('layout extractStyles' , true);
+app.set('layout extractScripts', true);
 
 // use express router
 
@@ -10,6 +21,7 @@ app.use('/' , require('./routes/index'));
 
 app.set('view engine' , 'ejs');
 app.set('views' , './views');
+
 
 app.listen(port , function(err){
     if(err){
