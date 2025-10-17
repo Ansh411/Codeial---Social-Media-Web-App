b6de05dc393727667fbf9c7766d32fa37dbf359f
Ansh411 <4gansh11@gmail.com>
2025-10-18 00:37:01 +0530

    Added user sign up and sign in view and controllers

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,19 @@ module.exports.profile = function(req,res){
         title : "User Profile"
     })
 }
+
+// renders the sign up page
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up', {
+        title : "Codeial | Sign up"
+    })
+}
+
+// renders the sign in page
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in', {
+        title : "Codeial | Sign in"
+    })
+}
