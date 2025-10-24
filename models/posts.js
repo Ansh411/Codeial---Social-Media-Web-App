c166fff6b0b9d587fdd4d66e89a16ba88202a14d
Ansh411 <4gansh11@gmail.com>
2025-10-24 14:58:22 +0530

    Update on home views

--- models/posts.js
+++ models/posts.js
@@ -10,10 +10,12 @@ const postSchema = new mongoose.Schema({
         ref: 'User'
     },
     // Include the array of ids of all comments in this post schema itself
-    comments: {
+    comments: [
+    {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Comment' 
     }
+ ]
 },{
     timestamps : true
 });
