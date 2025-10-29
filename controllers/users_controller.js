3313c963d7e3ce39fda94bb75e03b401b3f938f9
Ansh411 <4gansh11@gmail.com>
2025-10-29 23:28:19 +0530

    Fixed problem of profile pic overlapping and deletion of previous photo from localstorage and update it with the latest photo

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function(req,res){
     User.findById(req.params.id , function(err, user){
@@ -30,6 +32,13 @@ module.exports.update = async function(req,res){
                 user.email = req.body.email;
 
                 if(req.file){
+
+                    if(user.avatar){
+                        const avatarPath = path.join(__dirname, '..' , user.avatar);
+                        if(fs.existsSync(avatarPath)){
+                            fs.unlinkSync(avatarPath);
+                        }
+                    }
                     // This is saving the path of the uploaded file into the avatar field in the user
                     user.avatar = User.avatarPath + '/' + req.file.filename;
                 }
