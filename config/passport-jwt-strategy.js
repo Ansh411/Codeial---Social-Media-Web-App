335cc26b4781eb23ba8a85079489879790b92948
Ansh411 <4gansh11@gmail.com>
2025-10-31 00:46:06 +0530

    Update in passport-jwt

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -10,5 +10,21 @@ let opts = {
     secretOrKey : 'codeial'
 }
 
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
+
+    User.findById(jwtPayLoad._id, function(err, user){
+        if(err){
+            console.log('Error in finding user from JWT:', err);
+            return;
+        }
+        if(user){
+            return done(null, user);
+        }
+        else{
+            return done(null, false);
+        }
+    })
+
+}))
 
 module.exports = passport;
